efb991c3a86e164ab1a26e3df780267cc425bfd5
zero <artemlysakov793@gmail.com>
2024-06-30 12:03:11 +0300

    restart redax

--- RedaxTest/src/store.ts
+++ RedaxTest/src/store.ts
@@ -1,40 +1,29 @@
-import {configureStore} from '@reduxjs/toolkit'
+//Store - хранилище состояний вне приложения
+import { configureStore } from '@reduxjs/toolkit' // Встроенная функци которая создаёт наш Store
 
+//Опишим структуру состояния
 type State = {
     counter: number;
 };
 
-export type IncrementAction = {
-    type: 'increment'
-}
-export type DecrementAction = {
-    type: 'decrement'
-}
+//Вынесем типы данных в отдельные переменные
+export type  IncrementAction = { type:'increment'}
+export type  DecrementAction = { type:'decrement'}
 
-export type Action = IncrementAction | DecrementAction
-const initialState: State = {
+//Опишим структуру Action Добовления и Вычитания
+type Action = IncrementAction | DecrementAction
+
+//Мы создали действие-тригер (Action) теперь можем сделать наш reducer для счётчика
+const initialState: State = {   //Т.к при первой инициализации у нас нет дефолтного State мы создали базовое значение по умолчанию
     counter: 0,
-}
-
-const reducer = (state = initialState, action: Action): State => {
-    switch (action.type) {
-        case "increment": {
-            return {
-                ...state,
-                counter: state.counter + 1,
-            }
-        }
-        case "decrement": {
-            return {
-                ...state,
-                counter: state.counter - 1,
-            }
-        }
-        default: return state;
-
-    }
+};
+const reducer =(state = initialState, action: Action) : State => {
+    
 }
 
 export const store = configureStore({
-    reducer: reducer,
+    //reducer - функция которая говорит как должно менятся состояние в зависимости от Action
+    reducer: {
+
+    },
 })
