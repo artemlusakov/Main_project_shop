146185b80320bf3771dde4503e447ec689383f45
zero <artemlysakov793@gmail.com>
2024-06-30 14:35:37 +0300

    Work1 Redax
    базовый разбор компоненты Counter

--- RedaxTest/src/App.tsx
+++ RedaxTest/src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css'
+import {store, DecrementAction, IncrementAction} from './store'
+import {useEffect, useReducer} from "react";
 
 function App() {
+
+    const [, forceUpdate] = useReducer((x) => x + 1 ,0);
+
+    useEffect(()=>{
+        const unsubscribe = store.subscribe(()=>{
+            forceUpdate();
+        })
+        return unsubscribe;
+    },[])
+
     return (
         <div className='Main'>
-
+            <div className="counterBlock">
+                {/*dispatch - как пропс но необходим для отправки состояния */}
+                <button onClick={() => store.dispatch({type: "increment"} satisfies IncrementAction)}>+</button>
+                counter {store.getState().counter}
+                <button onClick={() => store.dispatch({type: "decrement"} satisfies DecrementAction)}>-</button>
+            </div>
         </div>
     )
 }
--- RedaxTest/src/store.ts
+++ RedaxTest/src/store.ts
@@ -1,5 +1,5 @@
 //Store - хранилище состояний вне приложения
-import { configureStore } from '@reduxjs/toolkit' // Встроенная функци которая создаёт наш Store
+import {configureStore} from '@reduxjs/toolkit' // Встроенная функци которая создаёт наш Store
 
 //Опишим структуру состояния
 type State = {
@@ -7,8 +7,8 @@ type State = {
 };
 
 //Вынесем типы данных в отдельные переменные
-export type  IncrementAction = { type:'increment'}
-export type  DecrementAction = { type:'decrement'}
+export type  IncrementAction = { type: 'increment' }
+export type  DecrementAction = { type: 'decrement' }
 
 //Опишим структуру Action Добовления и Вычитания
 type Action = IncrementAction | DecrementAction
@@ -17,13 +17,29 @@ type Action = IncrementAction | DecrementAction
 const initialState: State = {   //Т.к при первой инициализации у нас нет дефолтного State мы создали базовое значение по умолчанию
     counter: 0,
 };
-const reducer =(state = initialState, action: Action) : State => {
-    
+const reducer = (state = initialState, action: Action): State => {
+    // чтобы нам выполнить какое либо действие с состоянием нам нужно выловить тип Action в нашем случае это increment и decrement
+    switch (action.type) {       //будем менять состояния по типу actions
+
+        case "increment":
+            return {
+                ...state,   //сплитуем наш стэйт т.к суть в том что мы не должны его менять
+                counter: state.counter + 1 //Т.к мы засплитили нашь стэйт мы можем обращатся к его состояниям в данном случае оно одно и это состояние счётчика к которому мы приравниваем старое значение + 1
+            };
+
+        case "decrement":
+            return {
+                ...state,   //сплитуем наш стэйт т.к суть в том что мы не должны его менять
+                counter: state.counter - 1 //Т.к мы засплитили нашь стэйт мы можем обращатся к его состояниям в данном случае оно одно и это состояние счётчика к которому мы приравниваем старое значение - 1
+            };
+
+       default :
+           return state;  // Всегда делайте возвращение state в качестве дефолтного значения если ни один тип даных не подходит
+    }
+
 }
 
 export const store = configureStore({
     //reducer - функция которая говорит как должно менятся состояние в зависимости от Action
-    reducer: {
-
-    },
+    reducer:reducer,
 })
